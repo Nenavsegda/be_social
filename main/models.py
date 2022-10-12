2420d7fd59229984b630c09b963c589a2c066956
Dmitry <dimachizhikov2@gmail.com>
2022-10-12 18:46:16 +0300

    Добавлена модель LikePost

--- main/models.py
+++ main/models.py
@@ -28,3 +28,11 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.user_name} posted: {self.caption}'
+
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.username} liked {self.post_id}'
--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('like_post', views.like_post, name='like_post'),
 ]
