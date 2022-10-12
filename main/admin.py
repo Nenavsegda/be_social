88bd529d5c6956d5847217ebb3a6a9e1c797182a
Dmitry <dimachizhikov2@gmail.com>
2022-10-12 21:00:24 +0300

    Добавлена возможность читать других пользователей

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from main.models import LikePost, Post, Profile
+from main.models import FollowersCount, LikePost, Post, Profile
 
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(LikePost)
+admin.site.register(FollowersCount)
